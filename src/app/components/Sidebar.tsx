6b066d8b1ca9bf97919dbc441906c4ec3929160b
Chris Pfaff <chrispfaff10@gmail.com>
2023-06-28 17:21:06 -0500

    sidebar choose video modifier

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,25 +1,28 @@
 export default function SideBar({
   showSidebar,
   closeSidebar,
+  chooseOptions,
 }: {
   showSidebar: boolean;
   closeSidebar: <HTMLButtonElement>() => void;
+  chooseOptions: (option: string) => void;
 }) {
+  const options = ["Black & White", "Blur", "Stabalize", "Reverse", "Vignette"];
   return (
     <nav
-      className={`fixed -left-28 top-0 overflow-hidden z-10 bg-blue-500 min-h-screen pl-10 pr-10 pt-10 border-t-2 border-r-2 border-l-0 border-b-2 rounded-r-3xl ease-in-out duration-300
+      className={`fixed -left-28 top-0 overflow-hidden z-10 bg-blue-500 min-h-screen pr-10 pt-10 border-t-2 border-r-2 border-l-0 border-b-2 rounded-r-3xl ease-in-out duration-300
       ${showSidebar ? "translate-x-full" : ""}`}
     >
-      <div className="flex items-center justify-center mb-6">
+      <div className="flex  mb-6">
         <button onClick={closeSidebar}>
-          <div className="absolute left-0 flex items-center justify-center mb-6 w-full">
+          <div className="absolute left-2 flex text-xl mb-6 w-full">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-6 h-6 text-black cursor-pointer pb-2"
+              className="w-12 h-14 text-black cursor-pointer pb-2"
             >
               <path
                 strokeLinecap="round"
@@ -30,15 +33,22 @@ export default function SideBar({
           </div>
         </button>
       </div>
-      <ul className="relative m-0 list-none text-black">
-        <li className="p-2 font-medium ">Home</li>
-        <li className="p-2 font-medium">Modifier</li>
-        <li className="p-2 font-medium">Modifier</li>
-        <li className="p-2 font-medium">Modifier</li>
-        <li className="p-2 font-medium">Modifier</li>
-        <li className="p-2 font-medium">Modifier</li>
-        <li className="p-2 font-medium">Modifier</li>
-        <li className="p-2 font-medium">Modifier</li>
+      <ul className="relative m-0 pt-8 list-none text-black">
+        {options.map((item) => {
+          return (
+            <li className="p-2 font-medium" key={item}>
+              <button
+                data-item={item}
+                className="cursor-pointer"
+                onClick={(e) => {
+                  chooseOptions(e.currentTarget.dataset.item || "");
+                }}
+              >
+                {item}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
