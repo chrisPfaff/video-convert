3ca1de536e7ed06a8d0e1e56399291edcd7d92a2
Chris Pfaff <chrispfaff10@gmail.com>
2023-06-28 17:21:36 -0500

    added option to choose which ffmpegscript to run

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
   const [videoSrc, seVideoSrc] = useState("");
   const [result, setResult] = useState("");
   const [video, setVideo] = useState<File | null>(null);
+  const [options, setOptions] = useState("");
 
   const videoLoad = async () => {
     await ffmpeg.load();
@@ -25,10 +26,36 @@ export default function Home() {
       seVideoSrc(url);
     }
   };
-  const convertVideo = async () => {
+  const convertVideo = async (dataString: string) => {
     if (video) {
       ffmpeg.FS("writeFile", "test.mp4", await fetchFile(video));
-      await ffmpeg.run("-i", "test.mp4", "-vf", "format=gray", "output.mp4");
+
+      switch (options) {
+        case "Black & White":
+          await ffmpeg.run(
+            "-i",
+            "test.mp4",
+            "-vf",
+            "format=gray",
+            "output.mp4"
+          );
+          break;
+        case "Blur":
+          await ffmpeg.run("-i", "test.mp4", "-vf", "dblur", "output.mp4");
+          break;
+        case "Stabalize":
+          await ffmpeg.run("-i", "test.mp4", "-vf", "deshake", "output.mp4");
+          break;
+        case "Reverse":
+          await ffmpeg.run("-i", "test.mp4", "-vf", "reverse", "output.mp4");
+          break;
+        case "Vignette":
+          await ffmpeg.run("-i", "test.mp4", "-vf", "vignette", "output.mp4");
+          break;
+        default:
+          break;
+      }
+
       const data = ffmpeg.FS("readFile", "output.mp4");
       const url = URL.createObjectURL(
         new Blob([data.buffer], { type: "video/mp4" })
@@ -47,8 +74,18 @@ export default function Home() {
 
   return (
     <>
-      <SideBar showSidebar={showSidebar} closeSidebar={handleSideBar} />
-      <main className="before:absolute before:top-0 before:bottom-0 before:-left-32 before:right-[90%] before:border-t-2 before:border-r-2 before:border-l-0 before:border-b-2  before:bg-blue-500 flex min-h-screen flex-col items-center justify-center p-4 gap-4 overflow-auto relative">
+      <SideBar
+        showSidebar={showSidebar}
+        closeSidebar={handleSideBar}
+        chooseOptions={setOptions}
+      />
+      <main
+        className={` ${
+          showSidebar
+            ? "before:absolute before:top-0 before:bottom-0 before:-left-32 before:right-[95%] before:border-t-2 before:border-r-2 before:border-l-0 before:border-b-2  before:bg-blue-500"
+            : ""
+        } flex min-h-screen flex-col items-center justify-center p-4 gap-4 overflow-auto relative`}
+      >
         <div className="absolute left-24 top-12 flex items-center justify-end mb-6">
           <button
             onClick={() => {
@@ -70,9 +107,6 @@ export default function Home() {
           </button>
         </div>
         <div className="flex-col items-center justify-center w-96 h-auto">
-          {/* {videoSrc && (
-          <video src={videoSrc} controls width="350" height="350"></video>
-        )} */}
           {videoSrc ? (
             <video src={videoSrc} controls width="350" height="350"></video>
           ) : (
@@ -90,7 +124,7 @@ export default function Home() {
           />
         </form>
         <button
-          onClick={convertVideo}
+          onClick={() => convertVideo(options)}
           className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
         >
           convert
